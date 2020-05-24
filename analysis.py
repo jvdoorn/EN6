704e117cfb9cadc46127f7588c6b9fffe95f2a11
Julian van Doorn <jvdoorn@antarc.com>
2020-05-24 15:19:54 +0200

    Added various tools for more in depth analysis or to exclude certain analyses if they are not needed.

--- analysis.py
+++ analysis.py
@@ -3,14 +3,22 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
+from sklearn.metrics import mean_squared_error
 
-# Import entries, should be a list of tuples in the format (file name, rotation, error in rotation).
-from settings import entries
+# Import entries, should be a list of tuples in the format (file name, rotation, error in rotation)
+# and various configurations.
+from settings import compare, create_map, entries, map_values
+
+
+def remap(values: np.ndarray, minimum: float, maximum: float) -> np.ndarray:
+    return (values - minimum) / (maximum - minimum)
+
 
 data = np.array(entries)
 
-# Create necessary Matplotlib objects
-figure, axis = plt.subplots(len(entries), 2, tight_layout=True)
+if create_map:
+    # Create necessary Matplotlib objects
+    figure, axis = plt.subplots(len(entries), 2, tight_layout=True)
 
 # Load data
 rotations = data[:, 1].astype(np.float)
@@ -36,13 +44,14 @@ for i in range(len(entries)):
     # Determine the intensity of each pixel (this excludes alpha values.
     intensity = np.max(np.array(image)[:, :, :3], 2)
 
-    # Plot the raw image
-    axis[i, 0].imshow(image)
-    axis[i, 0].set_title(file_name)
+    if create_map:
+        # Plot the raw image
+        axis[i, 0].imshow(image)
+        axis[i, 0].set_title(file_name)
 
-    # Plot the intensity
-    axis[i, 1].imshow(intensity, cmap='gray')
-    axis[i, 1].set_title(f'{file_name} intensity')
+        # Plot the intensity
+        axis[i, 1].imshow(intensity, cmap='gray')
+        axis[i, 1].set_title(f'{file_name} intensity')
 
     # Save the intensities to their array
     mean_intensity = np.mean(intensity)
@@ -56,25 +65,60 @@ for i in range(len(entries)):
     print(
         f'TeX friendly: ${rotations[i]:.1e}\pm{rotations_err[i]:.0e}\degrees$ & ${mean_intensity:.1e}\pm{mean_intensity_err:.0e}$ \\\\')
 
-# Save the figure maps
-plt.savefig(f'figures/maps-{timestamp}.jpg')
-plt.show()
+if create_map:
+    # Save the figure maps
+    plt.savefig(f'figures/maps-{timestamp}.jpg')
+    plt.show()
 
 # Determine the maximum intensity
-I0 = np.max(intensities)
-# Calculate the relative intensities
-relative_intensities = intensities / I0
-# Determine the error in the relative intensities
-relative_intensities_err = (1 / I0) * np.sqrt(
-    np.square(intensities_err) + np.square(intensities_err[np.argmax(intensities)]) / np.square(I0))
+I_max = np.max(intensities)
+I_max_err = intensities_err[np.argmax(intensities)]
+if map_values:
+    # Determine the minimum intensity
+    I_min = np.min(intensities)
+    I_min_err = intensities_err[np.argmin(intensities)]
+    # Calculate the relative intensities
+    relative_intensities = remap(intensities, I_min, I_max)[:, 0]
+    # Determine the error in the relative intensities
+    relative_intensities_err = (np.sqrt((1 / (I_max - I_min) * intensities_err) ** 2 + (
+            (intensities - I_max) / (I_max - I_min) ** 2 * I_min_err) ** 2 + (
+                                                (I_min - intensities) / (I_max - I_min) ** 2 * I_max_err) ** 2))[:, 0]
+else:
+    # Calculate the relative intensities
+    relative_intensities = (intensities / I_max)[:, 0]
+    # Determine the error in the relative intensities
+    relative_intensities_err = ((1 / I_max) * np.sqrt(
+        np.square(intensities_err) + np.square(I_max_err) / np.square(I_max)))[:, 0]
 
 # Plot the intensity of rotation
 plt.title('Intensity vs. rotation')
 plt.grid()
-plt.errorbar(rotations, relative_intensities, relative_intensities_err, rotations_err)
+plt.errorbar(rotations, relative_intensities, relative_intensities_err, rotations_err, label='Measurement')
 plt.xlabel('Rotation [°]')
-plt.ylabel(r'Intensity ($I/I_0$) [unit less]')
+if map_values:
+    plt.ylabel(r'Relative intensity ($I$ mapped between $I_{max}$ and $I_{min}$) [unit less]')
+else:
+    plt.ylabel(r'Intensity ($I/I_0$) [unit less]')
+
+if compare:
+    samples = np.linspace(np.min(rotations), np.max(rotations), 500)
+    plt.plot(samples, np.cos(np.deg2rad(samples)) ** 2, label='Prediction')
+    plt.legend()
+
+    MSE = mean_squared_error(relative_intensities - np.cos(np.deg2rad(rotations)) ** 2, np.zeros(np.shape(rotations)))
+    print(f'MSE between measurement and prediction: {MSE:.1e}')
 
 # Save the plot
 plt.savefig(f'figures/plot-{timestamp}.svg')
 plt.show()
+
+if compare:
+    plt.title('Deviation from expected result')
+    plt.errorbar(rotations, relative_intensities - np.cos(np.deg2rad(rotations)) ** 2, relative_intensities_err)
+    plt.xlabel('Rotation [°]')
+    plt.ylabel(r'Deviation of relative intensity from expected value [unit less]')
+    plt.grid()
+
+    # Save the plot
+    plt.savefig(f'figures/plot-{timestamp}-c.svg')
+    plt.show()
